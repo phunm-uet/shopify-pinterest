1e01c8a1e78a0bc9c633cf798353f6e353616692
phunm-uet <phuapplehmu@gmail.com>
2018-05-11 15:14:19 +0700

    Upd

--- app/Console/Commands/PushFB.php
+++ app/Console/Commands/PushFB.php
@@ -3,7 +3,10 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-
+use GuzzleHttp\Client;
+use App\Product;
+use App\History;
+use Carbon\Carbon;
 class PushFB extends Command
 {
     /**
@@ -11,14 +14,14 @@ class PushFB extends Command
      *
      * @var string
      */
-    protected $signature = 'push:fb';
+    protected $signature = 'push:fb {collectionId}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Push random product in collection to FB';
 
     /**
      * Create a new command instance.
@@ -37,6 +40,27 @@ class PushFB extends Command
      */
     public function handle()
     {
-        //
+        // Get product to push
+        $product = Product::where('is_fb_publish',0)
+                            ->inRandomOrder()->first();
+        $accessToken = env('FB_PAGE_TOKEN');
+        if($product == null || $accessToken == null) die;
+        $client = new Client();
+        $body = [
+            "access_token" => $accessToken,
+            "caption" => $product['product_title'],
+            "message" => $product['product_title'],
+            "link" => $product['product_link'],
+            "image" => $product['product_image']
+        ];
+        try{
+            $r = $client->request('POST', 'https://graph.facebook.com/v3.0/me/feed', ['form_params' => $body]);
+            $body = json_decode($r->getBody(),true);
+            $postId = $body['id'];
+        }catch(\Exception $e){
+            DB::table('logs')->insert([
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 }
